c8740ef194ddab5e01083215985ca4faa19cd378
Nate Novak <nate.j.nov@gmail.com>
2022-04-30 20:40:56 -0400

    Refactored

--- include/markerless.h
+++ include/markerless.h
@@ -16,86 +16,48 @@
 #include <opencv2/opencv.hpp>
 
 /**
- * @brief Function to multiply each value in a matrix by 01
+ * @brief Function to find the model's keypoints and descriptors for those keypoints
  * 
- * @param matrix is the matrix to be multiplied by -1
+ * @param detector ORB detector 
+ * @param model input array for model image
+ * @param keypoints input array keypoints found on the image.
+ * @param descriptors descriptors for the keypoints found in the model. 
  */
-void invert_values(cv::Mat &matrix); 
+void get_model_kp_desc(cv::Ptr<cv::ORB> detector, cv::Mat &model, std::vector<cv::KeyPoint> &keypoints, cv::Mat &descriptors); 
 
 /**
- * @brief Function to multiply two matrices
+ * @brief Function to match keypoints in the scene and the model
  * 
- * @param mat1 One of the matrices to multiply
- * @param mat2 The other matrix to multiply 
- * @return cv::Mat result of the mulitplication 
+ * @param matcher DiscriptorMatcher that matches descriptors to the key points
+ * @param desc_scene input array of descriptors of keypoints in the scene
+ * @param desc_model input array of descriptors of keypoints in the model
+ * @param acceptable_matches output vector of the top matches between the scene and model
+ * @param enough boolean that indicates whether or not there are sufficient keypoint matches between the scene and boolean 
  */
-cv::Mat matrix_multiplication(cv::Mat &mat1, cv::Mat &mat2); 
+void match_kps(cv::Ptr<cv::DescriptorMatcher> matcher, cv::Mat &desc_scene, cv::Mat &desc_model, std::vector<cv::DMatch> &acceptable_matches, bool &enough); 
 
 /**
- * @brief Function to calculate the multiplicative inverse of a matrix
+ * @brief Function to get the rotations and translations from the solvePnP opencv function
  * 
- * @param matrix matrix to be inverted
- * @return cv::Mat result of inversion
+ * @param matches keypoint matches
+ * @param keypoints_model keypoints in the model
+ * @param keypoints_scene keypoints in the scene
+ * @param model the model image
+ * @param rotations output array of the rotations
+ * @param translations output array of the translations
+ * @param cam_mat camera matrix 
+ * @param dist_coeffs distortion coefficients
  */
-cv::Mat multiplicative_inverse(cv::Mat &matrix);
+void get_rots_and_trans(std::vector<cv::DMatch> &matches, std::vector<cv::KeyPoint> &keypoints_model, std::vector<cv::KeyPoint> &keypoints_scene, 
+                        cv::Mat &model, cv::Mat &rotations, cv::Mat &translations, cv::Mat cam_mat, cv::Mat dist_coeffs); 
 
 /**
- * @brief Get the column of a specific matrix as a 1D Mat
+ * @brief Function to draw the axes
  * 
- * @param matrix matrix from which to retrieve the column
- * @param colnum the column number
- * @return cv::Mat the output
+ * @param points vector of world points to write to
+ * @param origin origin of the axes
+ * @param scale scale of the axes
+ * @param color color of the axes
+ * @return int 
  */
-cv::Mat get_column(const cv::Mat &matrix, const int colnum); 
-
-/**
- * @brief Function to calculate the L2 norm of the vectors 
- * 
- * @param vector vector to determine the L2 norm of 
- * @return float result of the L2 norm calculation
- */
-float l2_norm(const cv::Mat vector); 
-
-/**
- * @brief Function to normalize a vector based on a normalization factor
- * 
- * @param vector vector to be normalized
- * @param norm_factor factor by which to normalize a vector
- * @return cv::Mat vector that holds normalized values
- */
-cv::Mat normalize_vector(const cv::Mat &vector, const float norm_factor); 
-
-/**
- * @brief Function do an element-wise addition of two vectors
- * 
- * @param vec1 one vector to be added
- * @param vec2 other vector to be added
- * @return cv::Mat result of element-wise addition
- */
-cv::Mat add_vectors(const cv::Mat &vec1, const cv::Mat &vec2); 
-
-/**
- * @brief Function to calculate the cross product of 2 3X1 vectors
- * 
- * @param vec1 one vectors of the cross product
- * @param vec2 the other vector of the cross product
- * @return cv::Mat the output vector of the cross product
- */
-cv::Mat cross_product(const cv::Mat &vec1, const cv::Mat &vec2); 
-
-/**
- * @brief Function do an element-wise subtraction of two vectors
- * 
- * @param vec1 one vector to be subtracted
- * @param vec2 other vector to be subtracted
- * @return cv::Mat result of element-wise subtraction
- */
-cv::Mat subtract_vectors(const cv::Mat &vec1, const cv::Mat &vec2); 
-
-/**
- * @brief Function to combine a vector of matrices into one unified matrix
- * 
- * @param mats matrices to "stack" 
- * @return cv::Mat "stacked" matrices
- */
-cv::Mat stack(std::vector<cv::Mat> &mats); 
+int axes_points(std::vector<cv::Vec3f> &points, cv::Vec3f origin, float scale); 
